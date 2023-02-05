03673d76b69be7ecf853a40e6c8733ce1ba8339f
Dmytro Davidenko <dima080885@gmail.com>
2023-02-05 20:28:27 +0200

    deploy fix, base route fix

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { store } from './redux/store/store';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="SlimMomApp">
       <Provider store={store}>
         <App />
       </Provider>
